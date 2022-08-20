b5ce12ef1e6f2d57ab869388c0046a5f1248e962
Antoniza <82987222+Antoniza@users.noreply.github.com>
2022-08-19 19:19:25 -0600

    Submit data to db

--- app/src/main/java/com/proyecto/pm01exameniiiparcial/MainActivity.java
+++ app/src/main/java/com/proyecto/pm01exameniiiparcial/MainActivity.java
@@ -3,6 +3,8 @@ package com.proyecto.pm01exameniiiparcial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -12,21 +14,46 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     FirebaseFirestore mFirestore;
+    EditText description, quantity, period, time;
+    Button save;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mFirestore = FirebaseFirestore.getInstance();
+        description = findViewById(R.id.txtDescripcion);
+        quantity = findViewById(R.id.txtCantidad);
+        period = findViewById(R.id.txtPeriodo);
+        time = findViewById(R.id.spTiempo);
+        save = findViewById(R.id.btnGuardar);
 
-        saveData();
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData();
+            }
+        });
 
     }
 
 
     private void saveData(){
-        Map<String, Object> map = new HashMap<>();
-        map.put("name", "Josue");
-        mFirestore.collection("User").document().set(map);
+        String mdescription = description.getText().toString();
+        String mquantity = quantity.getText().toString();
+        String mperiod = period.getText().toString();
+        String mtime = time.getText().toString();
+
+        if(mdescription.equals("") || mquantity.equals("") || mperiod.equals("") || mtime.equals("")){
+            Toast.makeText(this, "Se deben llenar todos los campos", Toast.LENGTH_LONG).show();
+        }else{
+            Map<String, Object> data = new HashMap<>();
+            data.put("description", mdescription);
+            data.put("quantity", mquantity);
+            data.put("period", mperiod);
+            data.put("time", mtime);
+
+            mFirestore.collection("Medicines").document().set(data);
+        }
     }
 }
